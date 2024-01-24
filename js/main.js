521bfb840c462ec99c686e8d2aede5001053b36a
SEIFELESLLAM SEIF <seifsaif151@gmail.com>
2024-01-24 20:07:20 +0300

    added video rules

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Get all elements with the "blured" class
-  var bluredElements = document.querySelectorAll(".blured");
+  var blurredElements = document.querySelectorAll(".blured");
 
   // Add a click event listener to each element
-  bluredElements.forEach(function (element) {
+  blurredElements.forEach(function (element) {
     element.addEventListener("click", function (event) {
       event.preventDefault(); // Prevent the default behavior of the link
       alert("Coming soon"); // Show an alert when clicked
     });
   });
 });
-
-console.log("Hello from main.js!");
