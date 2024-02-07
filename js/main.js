263860271af8151d85f7744876411457d5b2adee
SEIFELESLLAM SEIF <seifsaif151@gmail.com>
2024-02-07 23:49:50 +0300

    modified

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get all elements with the "blured" class
   var blurredElements = document.querySelectorAll(".blured");
 
-  // Add a click event listener to each element
   blurredElements.forEach(function (element) {
     element.addEventListener("click", function (event) {
-      event.preventDefault(); // Prevent the default behavior of the link
-      alert("Coming soon"); // Show an alert when clicked
+      event.preventDefault();
+      alert("Coming soon");
     });
   });
 });
