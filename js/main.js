93d0836fa58cf0049febed20bfcbbc01d1f842f9
SEIFELESLLAM SEIF <seifsaif151@gmail.com>
2024-02-09 12:44:32 +0300

    duration fixed

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         text: "Coming soon",
         destination:
           "https://www.instagram.com/skam.cards?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
-        duration: 3000000000000,
+        duration: 300,
         position: "center",
         style: {
           background: "#d3414d",
