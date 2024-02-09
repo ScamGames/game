6b6d38c45623db3c0a3ca9aed0ecf2ae35954b6c
SEIFELESLLAM SEIF <seifsaif151@gmail.com>
2024-02-09 12:19:16 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var blurredElements = document.querySelectorAll(".blured");
+  var blurredElements = document.querySelectorAll(".blurred");
 
   blurredElements.forEach(function (element) {
     element.addEventListener("click", function (event) {
       event.preventDefault();
-      alert("Coming soon");
+      Toastify({
+        text: "Coming soon",
+        destination:
+          "https://www.instagram.com/skam.cards?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+        duration: 3000000000000,
+        position: "center",
+        style: {
+          background: "#d3414d",
+          borderRadius: "10px",
+          padding: "10px 20px",
+        },
+      }).showToast();
     });
   });
 });
